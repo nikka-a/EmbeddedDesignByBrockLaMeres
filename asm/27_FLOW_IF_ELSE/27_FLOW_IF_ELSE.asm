0851b1d39efea94d32ba7181a66ad6541ff5a0fa
nikka <63124504+nikka-a@users.noreply.github.com>
2026-04-18 21:36:53 +0300

    Create 27_FLOW_IF_ELSE.asm

---
+++ asm/27_FLOW_IF_ELSE/27_FLOW_IF_ELSE.asm
@@ -0,0 +1,56 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls  C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def     RESET                   ; Export program entry-point to
+                                             ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                            ; Assemble into program memory.
+            .retain                          ; Override ELF conditional linking
+                                             ; and retain current section.
+            .retainrefs                      ; And retain any sections that have
+                                             ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w    #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w    #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+            mov.w    #0, R15                ; move 0 to R15
+
+while:
+
+if:
+            cmp.w    #10, R15               ; R15 - 10
+            jnz      else                   ; if Z flag was not set (R15!=10), jump to else
+            mov.w    #0, R15                ; put 0 to R15
+            jmp      end_if                 ; go to end_if
+
+else:
+            inc      R15                    ; increment R15
+
+end_if:
+
+end_while:
+
+            jmp      while
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global  __STACK_END
+            .sect    .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect    ".reset"                ; MSP430 RESET Vector
+            .short   RESET
